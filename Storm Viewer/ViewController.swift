84c473ee4dfa46f0b675908e2fdb16ac610da641
Aditya Vyavahare <vvaditya23@icloud.com>
2023-10-11 19:43:14 +0530

    Added tableview.

--- Storm Viewer/ViewController.swift
+++ Storm Viewer/ViewController.swift
@@ -9,10 +9,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let VC = UIViewController()
+    let tableView = UITableView()
+    
     var pictures: [String] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        let navigationController = UINavigationController(rootViewController: VC)
+        
+        setUpTableView()
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "Picture")
+        self.view.addSubview(tableView)
+        
         let fm = FileManager.default
         let path = Bundle.main.bundlePath
         let items = try! fm.contentsOfDirectory(atPath: path)
@@ -25,7 +34,27 @@ class ViewController: UIViewController {
         }
 //        print(pictures)
     }
-
-
 }
 
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func setUpTableView() {
+        // Get main screen bounds
+        let screenSize: CGRect = UIScreen.main.bounds
+                
+                let screenWidth = screenSize.width
+                let screenHeight = screenSize.height
+         
+                tableView.frame = CGRectMake(0, 0, screenWidth, screenHeight);
+                tableView.dataSource = self
+                tableView.delegate = self
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+    }
+}
