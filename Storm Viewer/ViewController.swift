654ae3b0d632108b2bd3802c63ad135a3fc24a00
Aditya Vyavahare <vvaditya23@icloud.com>
2023-10-11 19:19:28 +0530

    Added logic to read pictures using file manager.

--- Storm Viewer/ViewController.swift
+++ Storm Viewer/ViewController.swift
@@ -9,10 +9,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var pictures: [String] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        view.backgroundColor = .yellow
+        let fm = FileManager.default
+        let path = Bundle.main.bundlePath
+        let items = try! fm.contentsOfDirectory(atPath: path)
+        
+        for item in items {
+            //picture loaded
+            if item.hasPrefix("nssl") {
+                pictures.append(item)
+            }
+        }
+//        print(pictures)
     }
 
 
